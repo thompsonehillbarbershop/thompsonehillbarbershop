76b529e7ea2c4576cbab56e124e75cca10158e99
Thiago Elias <thiagoelias99@gmail.com>
2025-04-26 18:09:33 -0300

    layout fix

--- web/src/app/app/(totem)/layout.tsx
+++ web/src/app/app/(totem)/layout.tsx
@@ -3,15 +3,16 @@ import { PropsWithChildren } from "react"
 
 export default function TotemLayout({ children }: PropsWithChildren) {
   return (
-    <div className={`w-screen h-screen bg-[url(/images/background.webp)] bg-no-repeat bg-cover`}>
-      <div className="w-full h-full flex flex-col items-center justify-center gap-4 bg-background/95">
-        <div className="w-full h-full flex flex-col gap-4 justify-start items-center px-8 pt-10 pb-28">
+    <div className={`w-screen h-screen bg-[url(/images/background.webp)] bg-no-repeat bg-cover flex flex-col`}>
+      <div className="w-full flex-1 h-full flex flex-col items-center justify-center gap-4 bg-background/95">
+        <div className="w-full h-full flex flex-col gap-4 justify-start items-center px-8 pt-6 pb-14">
           <Image
             src="/logo.png"
             className="h-56"
             alt="Logo Thompson Hill"
             width={300}
             height={300}
+
           />
           {children}
         </div>
