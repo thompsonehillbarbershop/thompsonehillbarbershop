48b4f25a0b48e46308fdb806a395dd45e4d26f8e
Thiago Elias <thiagoelias99@gmail.com>
2025-04-28 16:19:46 -0300

    add customer photo

--- web/src/components/totem/customer-register-form.tsx
+++ web/src/components/totem/customer-register-form.tsx
@@ -5,15 +5,16 @@ import { z } from "@/lib/pt-zod"
 import { applyDateMask, applyPhoneMask, cn, formatPhoneToE164, isDateValid } from "@/lib/utils"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useForm } from "react-hook-form"
 import { VirtualKeyboard } from "../ui/virtual-keyboard"
 import { Button } from "../ui/button"
-import { ChevronRight } from "lucide-react"
+import { CameraIcon, ChevronRight } from "lucide-react"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 import { Input } from "../ui/input"
 import { EGender } from "@/models/customer"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import Image from "next/image"
 
 const formSchema = z.object({
   givenName: z.string().nonempty({ message: "Obrigatório" }),
@@ -29,6 +30,8 @@ export default function CustomerRegisterForm() {
   const [activeField, setActiveField] = useState<keyof z.infer<typeof formSchema> | null>(null)
   const [keyBoardLayout, setKeyboardLayout] = useState<"qwerty" | "numpad">("qwerty")
   const router = useRouter()
+  const invoiceInputRef = useRef<HTMLInputElement>(null)
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
   const phoneNumber = searchParams.get('tel')
 
@@ -54,6 +57,7 @@ export default function CustomerRegisterForm() {
 
   useEffect(() => {
     form.setFocus("givenName")
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -82,79 +86,151 @@ export default function CustomerRegisterForm() {
     }
   }
 
+  async function handlePhotoInput(file: File) {
+    setSelectedFile(file)
+    console.log("Photo input", file)
+  }
+
   return (
     <div className="w-full flex flex-col flex-1 justify-start items-center">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col gap-2 md:gap-6 max-w-lg lg:w-lg">
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem className="space-y-0.5 hidden">
-                <FormLabel className="sm:text-xl md:text-2xl">Telefone</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    maxLength={15}
-                    placeholder="(00) 00000-0000"
-                    disabled
-                    className="w-full text-center sm:text-xl md:text-2xl"
-                    {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="w-full flex justify-between items-start gap-4">
-            <FormField
-              control={form.control}
-              name="givenName"
-              render={({ field }) => (
-                <FormItem className="space-y-0.5 w-full">
-                  <FormLabel className="sm:text-xl md:text-2xl">Nome</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      readOnly
-                      // autoFocus
-                      onFocus={() => {
-                        setKeyboardLayout("qwerty")
-                        setActiveField("givenName")
-                      }}
-                      className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "givenName" ? "border-ring ring-ring/50 ring-[3px]" : "")}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col gap-2 md:gap-6 max-w-2xl">
+          <div className="flex flex-row justify-start items-start gap-4">
+            <div className="size-64 pt-4">
+              <input
+                autoFocus={false}
+                id="file"
+                name="file"
+                type="file"
+                className="hidden"
+                accept="image/*"
+                capture="environment"
+                ref={invoiceInputRef}
+                onChange={(e) => {
+                  const file = e.target.files?.[0]
+                  if (file) {
+                    handlePhotoInput(file)
+                  }
+                }}
+              />
+              {selectedFile ? (
+                <Image
+                  width={256}
+                  height={256}
+                  src={URL.createObjectURL(selectedFile)}
+                  alt="Foto capturada"
+                  className="w-full h-full object-cover rounded-lg"
+                  onClick={() => invoiceInputRef.current?.click()}
+                />
+              ) : (
+                <Button
+                  autoFocus={false}
+                  type="button"
+                  variant="outline"
+                  className="w-full size-64"
+                  onClick={() => invoiceInputRef.current?.click()}
+                >
+                  <CameraIcon className="size-24 stroke-[1.5px]" />
+                  <span className="ml-2"></span>
+                </Button>
               )}
-            />
-            <FormField
-              control={form.control}
-              name="familyName"
-              render={({ field }) => (
-                <FormItem className="space-y-0.5 w-full">
-                  <FormLabel className="sm:text-xl md:text-2xl">Sobrenome</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      readOnly
-                      onFocus={() => {
-                        setKeyboardLayout("qwerty")
-                        setActiveField("familyName")
-                      }}
-                      className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "familyName" ? "border-ring ring-ring/50 ring-[3px]" : "")}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            </div>
+
+            <div className="flex-1 flex flex-col gap-6 md:gap-8">
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem className="space-y-0.5 hidden">
+                    <FormLabel className="sm:text-xl md:text-2xl">Telefone</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        maxLength={15}
+                        placeholder="(00) 00000-0000"
+                        disabled
+                        className="w-full text-center sm:text-xl md:text-2xl"
+                        {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="w-full flex justify-between items-start gap-4">
+                <FormField
+                  control={form.control}
+                  name="givenName"
+                  render={({ field }) => (
+                    <FormItem className="space-y-0.5 w-full">
+                      <FormLabel className="sm:text-xl md:text-2xl">Nome e Sobrenome</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          readOnly
+                          // autoFocus
+                          onFocus={() => {
+                            setKeyboardLayout("qwerty")
+                            setActiveField("givenName")
+                          }}
+                          className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "givenName" ? "border-ring ring-ring/50 ring-[3px]" : "")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="familyName"
+                  render={({ field }) => (
+                    <FormItem className="space-y-0.5 w-full hidden">
+                      <FormLabel className="sm:text-xl md:text-2xl">Sobrenome</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          readOnly
+                          onFocus={() => {
+                            setKeyboardLayout("qwerty")
+                            setActiveField("familyName")
+                          }}
+                          className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "familyName" ? "border-ring ring-ring/50 ring-[3px]" : "")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="birthDate"
+                render={({ field }) => (
+                  <FormItem className="space-y-0.5">
+                    <FormLabel className="sm:text-xl md:text-2xl">Data de Nascimento</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        maxLength={10}
+                        placeholder="dd/mm/aaaa"
+                        readOnly
+                        onFocus={() => {
+                          setKeyboardLayout("numpad")
+                          setActiveField("birthDate")
+                        }}
+                        className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "birthDate" ? "border-ring ring-ring/50 ring-[3px]" : "")}
+                        {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
           </div>
           <FormField
             control={form.control}
             name="gender"
             render={({ field }) => (
-              <FormItem className="space-y-3">
+              <FormItem className="space-y-3 w-full max-w-sm self-center pt-4">
                 <FormLabel className="sm:text-xl md:text-2xl">Gênero</FormLabel>
                 <FormControl>
                   <RadioGroup
@@ -186,32 +262,9 @@ export default function CustomerRegisterForm() {
           />
           <FormField
             control={form.control}
-            name="birthDate"
-            render={({ field }) => (
-              <FormItem className="space-y-0.5">
-                <FormLabel className="sm:text-xl md:text-2xl">Data de Nascimento</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    maxLength={10}
-                    placeholder="dd/mm/aaaa"
-                    readOnly
-                    onFocus={() => {
-                      setKeyboardLayout("numpad")
-                      setActiveField("birthDate")
-                    }}
-                    className={cn("w-full text-center sm:text-xl md:text-2xl", activeField === "birthDate" ? "border-ring ring-ring/50 ring-[3px]" : "")}
-                    {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
             name="indicationCode"
             render={({ field }) => (
-              <FormItem className="space-y-0.5 w-full">
+              <FormItem className="space-y-0.5 w-full max-w-sm self-center">
                 <FormLabel className="sm:text-xl md:text-2xl">Possui código de indicação?</FormLabel>
                 <FormControl>
                   <Input
