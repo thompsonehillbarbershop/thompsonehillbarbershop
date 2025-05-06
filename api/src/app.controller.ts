eff953cd3ca85044b6acda7dae1cee0bff4efe99
Thiago Elias <thiagoelias99@gmail.com>
2025-05-06 17:15:19 -0300

    set server ping

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,15 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { ApiOperation, ApiResponse } from "@nestjs/swagger"
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
+
+  @Get("ping")
+  @ApiOperation({ summary: "Ping the server" })
+  @ApiResponse({ status: 200, description: "Pong" })
+  ping() {
+    return "Pong"
+  }
 }
