40357763fb553a0f4d4e1b0e16e7d52e039d57ac
Thiago Elias <thiagoelias99@gmail.com>
2025-06-11 09:19:45 -0300

    show all appointments

--- web/src/app/(app)/app/(attendant)/atendimento/fila/contents.tsx
+++ web/src/app/(app)/app/(attendant)/atendimento/fila/contents.tsx
@@ -1,24 +1,35 @@
 "use client"
 
 import AttendanceAppointmentCard from "@/components/attendant/appointment-card"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
 import { useAttendant } from "@/hooks/use-attendant"
 import { useQueue } from "@/hooks/useQueue"
 import { EPages } from "@/lib/pages.enum"
 import { EAppointmentStatuses } from "@/models/appointment"
 import { IFirebaseAppointment } from "@/models/firebase-appointment"
 import { useRouter } from "next/navigation"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 
 export default function AttendantQueuePageContents({ userId }: { userId: string }) {
+  const [showAll, setShowAll] = useState(false)
   const queue = useQueue()
   const { startAttendance, isStartingAttendance } = useAttendant()
   const router = useRouter()
 
   const userQueue = useMemo(() => {
+    // If showAll is true, return all appointments
+    if (showAll) {
+      const allAppointments = Object.values(queue).flat().filter(appointment => appointment.status === EAppointmentStatuses.WAITING || (appointment.status === EAppointmentStatuses.ON_SERVICE && appointment.attendant?.id === userId))
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      return allAppointments.sort((a, b) => a.status === EAppointmentStatuses.ON_SERVICE ? -1 : 1).sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+    }
+
     // Check if the user has an official queue
     const officialQueue = queue[userId] || []
     if (officialQueue.filter(appointment => appointment.status === EAppointmentStatuses.WAITING || appointment.status === EAppointmentStatuses.ON_SERVICE).length > 0) {
-      return officialQueue.filter(appointment => appointment.status === EAppointmentStatuses.WAITING || appointment.status === EAppointmentStatuses.ON_SERVICE)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      return officialQueue.filter(appointment => appointment.status === EAppointmentStatuses.WAITING || appointment.status === EAppointmentStatuses.ON_SERVICE).sort((a, b) => a.status === EAppointmentStatuses.ON_SERVICE ? -1 : 1)
     }
 
     // Check if general queue has waiting appointments
@@ -38,7 +49,7 @@ export default function AttendantQueuePageContents({ userId }: { userId: string 
 
     // If no appointments found, return an empty array
     return []
-  }, [queue, userId])
+  }, [queue, userId, showAll])
 
   async function onAttendanceStart(appointment: IFirebaseAppointment) {
     const response = await startAttendance({
@@ -58,6 +69,22 @@ export default function AttendantQueuePageContents({ userId }: { userId: string 
 
   return (
     <div className="w-full space-y-2">
+      <div className="w-full flex items-center justify-between">
+        <h1 className="text-2xl sm:text-3xl font-bold">Fila de Atendimento</h1>
+        <div className="flex justify-end items-center gap-2">
+          <Checkbox
+            id="showAll"
+            className="size-7"
+            checked={showAll}
+            onCheckedChange={() => setShowAll(!showAll)}
+          />
+          <Label
+            htmlFor="showAll"
+            className="sm:text-lg"
+          >Todos os atendimentos</Label>
+
+        </div>
+      </div>
       {userQueue.length === 0 && (
         <div className="text-center text-2xl sm:text-3xl font-bold">
           Não há atendimentos na fila
